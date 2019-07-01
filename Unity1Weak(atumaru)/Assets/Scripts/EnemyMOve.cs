125df40328f1cae1a0271856e2a5c461f9bcebee
hayabusa09060 <hayabusableach@gmail.com>
2019-07-01 22:50:42 +0900

    色々追加

---
+++ Unity1Weak(atumaru)/Assets/Scripts/EnemyMOve.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMOve : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 tragetpos;
+
+    public float speed,rotatspeed;
+    public float attenuation;
+    private float dis;
+    float x, y, z;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateMove();
+
+        Move();
+
+    }
+    void Move()
+    {
+        //transform.position += new Vector3(Mathf.Sin(Time.time * speed), 0, Mathf.Cos(Time.time * speed));
+
+        tragetpos += (player.transform.position - transform.position) * speed;
+        tragetpos *= attenuation;
+        transform.position += tragetpos *= Time.deltaTime;
+        //transform.position += new Vector3(Mathf.Sin(Time.time * speed), 0, Mathf.Cos(Time.time * speed));
+    }
+    void RotateMove()
+    {
+        Vector3 ppos = player.transform.position;
+        Vector3 enepos = this.transform.position;
+        dis = Vector3.Distance(enepos, ppos);
+
+        //// transform.position += new Vector3(dis * Mathf.Sin(Time.time * rotatspeed), 0, dis * Mathf.Cos(Time.time * rotatspeed));
+        // x = dis * Mathf.Sin(Time.time * rotatspeed);
+        // z = dis * Mathf.Cos(Time.time * rotatspeed);
+        // transform.position = new Vector3(x, 0, z);
+        // Debug.Log(dis);
+        rigidbody.MovePosition(new Vector3(dis * Mathf.Sin(Time.time * rotatspeed), transform.position.y, dis * Mathf.Cos(Time.time * rotatspeed)));
+    }
+}
---
+++ Unity1Weak(atumaru)/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        this.transform.position += new Vector3(x*speed, 0, y*speed); 
+        //rigidbody.AddForce(x*speed, 0, y*speed);
+    }
+}
