690fd559da01f4682484b6fc15c77b2f5b786f32
大石　隼 <>
2019-07-03 15:47:41 +0900

    スポーン途中生成

---
+++ Unity1Weak(atumaru)/Assets/Scripts/spon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spon : MonoBehaviour
+{
+    public GameObject[] enemy,ulepon;
+    public Vector3[] spornposision;
+    public int spornint; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void sporn()
+    {
+
+    }
+}
